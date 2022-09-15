11562c38eebf1508a6b51b4df2e796c947d25358
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-09-15 13:30:03 -0700

    main.h

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdl.1b.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "programing is positive, zero, or negative
+ * Return: 0
+ */
+int main{void)
+{
+int n;
+srand(time(0));
+n=rand() - RAND_MAX / 2;
+ /* your code goes there */
+if (n > 0)
+{
+        printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+        printf("%i is zeron\n",);
+}
+else if (n < 0)
+{
+        printf("%i is negative\n", n);
+}
+return (0);
+}
