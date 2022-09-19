7e426a6a0b17e772c1c9b87461031094c43fefd0
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-09-19 08:49:57 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,10 @@
  * @s: input string.
  * Return: length of a string.
  */
-
 int _strlen(char *s)
 {
 	int count = 0;
   	while (*(s + count) != '\0')
-	len++;
-	return (len);
+	count++;
+	return (count);
 }
