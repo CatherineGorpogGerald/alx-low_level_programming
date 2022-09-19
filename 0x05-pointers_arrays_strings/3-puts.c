01ee60a05d52b87ead4bbf42d44e41e98e78cf03
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-09-19 09:32:41 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,19 +6,17 @@
  * Return: no return.
  */
 void _puts(char *str)
-{
-	int count = 0;
+{	int count = 0;
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
- 
-		{
-			_putchar('\n');
-		break;
+	  if (str[count] == '\0')
+	{
+	_putchar('\n');
+	break;
 
-		}
-		_putchar(str[count]);
+	}
+	_putchar(str[count]);
 
-		count++;
+	count++;
 	}
 }
