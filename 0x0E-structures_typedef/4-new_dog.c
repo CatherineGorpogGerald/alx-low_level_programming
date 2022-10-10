1c8bfc0240f08fadbc111979bfb1492cfd6a78f3
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-10-10 14:53:37 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-
 #include <stdlib.h>
 
 /**
@@ -19,7 +18,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (p_dog == NULL || !(name) || !(owner))
 	{
 		free(p_dog);
-
 	return (NULL);
 	}
 	for (lname = 0; name[lname]; lname++)
@@ -35,16 +33,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
 	p_dog->name[i] = '\0';
 	p_dog->age = age;
-
 	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
-
 	p_dog->owner[i] = '\0';
-
 	return (p_dog);
 }
