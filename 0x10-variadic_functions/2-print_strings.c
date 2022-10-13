46a776a59cb1d412fb09b33914ce629522fb47da
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-10-13 13:04:29 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,21 +7,13 @@
  *
  *Return: no return.
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-
 	va_list valist;
-
 	unsigned int i;
-
 	char *str;
 
-
-
 	va_start(valist, n);
-
-
 
 	for (i = 0; i < n; i++)
 
@@ -29,12 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		str = va_arg(valist, char *);
 
-
-
 		if (str)
 
 			printf("%s", str);
-
 		else
 
 			printf("(nil)");
