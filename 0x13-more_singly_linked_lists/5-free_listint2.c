44f1d93ace04028151550a28085c19cac9745a31
CatherineGorpogGerald <catherinegerald88@gmail.com>
2022-10-26 13:17:22 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- *free_listint2 - frees a linked list
- *@head: head of a list.
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
  *
- *Return: no return.
+ * Return: no return.
  */
-
 void free_listint2(listint_t **head)
-{	listint_t *temp;
+{
+	listint_t *temp;
 	listint_t *curr;
 
 	if (head != NULL)
